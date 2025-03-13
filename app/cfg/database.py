58219984bfa25c53603449e5a18d3224aca0a7a4
klishinoleg <klishinoleg@gmail.com>
2025-03-13 13:45:12 +0300

    Add aws secrets and .env.default

--- app/cfg/database.py
+++ app/cfg/database.py
@@ -1,14 +1,35 @@
+import json
 import os
 from dotenv import load_dotenv
 from tortoise import Tortoise
+import boto3
 
 load_dotenv()
 
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_NAME = os.getenv('DB_NAME')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
+
+class Secret:
+    secrets: dict = {}
+
+    def __init__(self):
+        """Intit AWS sectrets if it's possible"""
+        aws_secret_name = os.getenv("AWS_SECRET_NAME")
+        aws_region = os.getenv("AWS_REGION")
+        if aws_region and aws_secret_name:
+            client = boto3.client("secretsmanager", aws_region)
+            secret_value = client.get_secret_value(SecretId=aws_secret_name)
+            self.secrets = json.loads(secret_value["SecretString"])
+
+    def get(self, key):
+        return self.secrets.get(key) or os.getenv(key)
+
+
+secrets = Secret()
+
+DB_USER = secrets.get('POSTGRES_USER')
+DB_PASSWORD = secrets.get('POSTGRES_PASSWORD')
+DB_NAME = secrets.get('DB_NAME')
+DB_HOST = secrets.get('DB_HOST')
+DB_PORT = secrets.get('DB_PORT')
 
 DATABASE_URL = f"postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
